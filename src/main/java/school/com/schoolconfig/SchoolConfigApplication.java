15e66db2fb53071bfd4629094247c5b6e550f6e8
Huaweini <Huaweini@users.noreply.github.com>
2019-07-24 21:56:45 +0800

    spring-boot目录结构

---
+++ src/main/java/school/com/schoolconfig/SchoolConfigApplication.java
@@ -0,0 +1,13 @@
+package school.com.schoolconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolConfigApplication.class, args);
+    }
+
+}
